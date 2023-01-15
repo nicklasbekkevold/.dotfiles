e38f55dd8066f9f516a4638f97def697e937f118
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-01-15 18:43:32 +0100

    Integrate new scripts in main startup.sh scripts

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -2,4 +2,5 @@
 
 sudo bash programs/python.sh
 sudo bash programs/node.sh
+sudo bash programs/gh.sh
 sudo bash programs/zsh.sh
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 sudo bash ./aptinstall.sh
 sudo bash ./programs.sh
+sudo bash ./fonts.sh
 
 # Get all upgrades
 sudo apt upgrade -y
